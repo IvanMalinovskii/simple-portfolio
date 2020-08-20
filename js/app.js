28858da5c0bb67d38884f42a5599c00e107b3a06
My Name <olgakaras@gmail.com>
2020-08-20 14:49:28 +0300

    +projects section

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
 
 new WOW().init();
+
+let swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.projects__pagination',
+        bulletClass: 'projects__bullet',
+        bulletActiveClass: 'projects__bullet_active',
+        clickable: true
+    }
+});
